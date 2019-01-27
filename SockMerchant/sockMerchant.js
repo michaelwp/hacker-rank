faf2922bf60b80e4e3997c7f6cf3e737f4e0eb65
kei-001 <michael.wenceslaus@gmail.com>
2019-01-27 20:14:15 +0700

    update all

--- SockMerchant/sockMerchant.js
+++ SockMerchant/sockMerchant.js
@@ -1,34 +1,35 @@
 'use strict';
-function matchPair(ar){
+function sockMerchant(n, ar) {
     let c = {};
     let d = 0;
     ar.forEach(e => {
         ar.forEach(f => {
-            if (e == f){
+            if (e == f) {
                 d++;
             }
         });
 
-        if (d > 1){
-            if ((d % 2) != 0){
+        if (d > 1) {
+            if ((d % 2) != 0) {
                 d -= 1;
             }
             d /= 2;
-            c[e]=d;
+            c[e] = d;
         }
         d = 0;
     });
 
     let r = 0;
     let _;
-    for (_ in c){
+    for (_ in c) {
         r += c[_];
     }
     return r;
 }
 
 //test case
+let n = 9
 let arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 //arr = [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]
 
-console.log(matchPair(arr));
+console.log(sockMerchant(n, arr));
