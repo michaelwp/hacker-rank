81354ac7cd57e0602e439de0079e81eb29b9121c
kei-001 <michael.wenceslaus@gmail.com>
2019-02-09 14:41:37 +0700

    update repeated string

--- RepeatedString/RepStr.py
+++ RepeatedString/RepStr.py
@@ -13,9 +13,6 @@ s = 'bcbccacaacbbacabcabccacbccbababbbbabcccbbcbcaccababccbcbcaabbbaabbcaabbbbbb
 n = 649606239668
 <<<<<<< HEAD
 # the result should be = 162401559918
-=======
-# the result should be = 162 401 559 918
->>>>>>> ec37d8786fb0bd5639d0a97c0599b0d1ab91b94d
 print(repeatedString(s, n))
 
 
