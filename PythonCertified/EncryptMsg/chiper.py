563750a5b1fa1f30410c9912a5b464e8ec6cf581
kei-001 <michael.wenceslaus@gmail.com>
2019-03-10 01:19:36 +0700

    chiper

--- PythonCertified/EncryptMsg/chiper.py
+++ PythonCertified/EncryptMsg/chiper.py
@@ -1,34 +1,57 @@
 
 class chiper():
-    def encrypt(m):
+    abj = 'abcdeghijklmnopqrstuvwxyz'
+    abx = abj.upper()
+    def encrypt(m, k=1):
         cipher = ''
         for char in m:
-            if not char.isalpha():
-                continue
-            char = char.upper()
-            code = ord(char) + 1
-            if code > ord('Z'):
-                code = ord('A')
-            cipher += chr(code)
+            if char in list(chiper.abj):
+                code = chiper.abj.index(char) + k
+                if code > 24:
+                    code = code-25
+                elif code < -25:
+                    code = code+25
+                cchar = chiper.abj[code]
+            elif char in list(chiper.abx):
+                code = chiper.abx.index(char) + k
+                if code > 24:
+                    code = code-25
+                elif code < -25:
+                    code = code+25
+                cchar = chiper.abx[code]
+            else:
+                cchar = char
+            cipher += cchar
         return(cipher)
 
-    def decrypt(c):
+    def decrypt(m, k=1):
         text = ''
-        for char in c:
-            if not char.isalpha():
-                continue
-            char = char.upper()
-            code = ord(char) - 1
-            if code < ord('A'):
-                code = ord('Z')
-            text += chr(code)
+        for char in m:
+            if char in list(chiper.abj):
+                code = chiper.abj.index(char) - k
+                if code > 24:
+                    code = code-25
+                elif code < -25:
+                    code = code+25
+                cchar = chiper.abj[code]
+            elif char in list(chiper.abx):
+                code = chiper.abx.index(char) - k
+                if code > 24:
+                    code = code-25
+                elif code < -25:
+                    code = code+25
+                cchar = chiper.abx[code]
+            else:
+                cchar = char
+            text += cchar
         return(text)
 
 
 # Encrypting Caesar cipher
 msg = input("Enter your message: ")
-print(chiper.encrypt(msg))
+key = int(input("Enter your key: ")) 
+print(chiper.encrypt(msg, key))
 
 # Caesar cipher - decrypting a message
-code = input('Enter your cryptogram: ')
-print(chiper.decrypt(code))
+chip = input('Enter your cryptogram: ')
+print(chiper.decrypt(chip, key))
