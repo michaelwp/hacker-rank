63df5c0805fd4102aba90146157b56a71c01725c
kei-001 <michael.wenceslaus@gmail.com>
2019-03-09 15:02:47 +0700

    led

--- PythonCertified/led/led.py
+++ PythonCertified/led/led.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 
 def leds(n=0):
     led = [ ('###','# #','# #','# #','###'),
@@ -10,20 +11,26 @@ def leds(n=0):
             ('###','  #','  #','#  ','  #'),
             ('###','# #','###','# #','###'),
             ('###','# #','###','  #','###')]
-    
+    #print('\n') ## whitespace
     for _ in range(5):
         for s in n:
-            print(led[int(s)][_], end=' ')
+            print (colored(led[int(s)][_], color='green'), end=' ')
         print('')
 
 def ledq():
     try:
-        n = (input("Input number (>= 0), default (0) : ")) or 0
-        q = list(str(n))
+        x = (input(colored("Input number (>= 0), default (0) : ",color="yellow"))) or 0
+        assert int(x) >= 0
+        q = list(str(x))
         leds(q)
     except ValueError:
-        print("The Number you entered is Invalid")
+        print(colored("The Number you entered is Invalid\n", color="red"))
+        ledq()
+    except AssertionError:
+        print(colored("The Number must >= 0\n","lightred"))
         ledq()
 
 if __name__ == "__main__":
+    print('\n') ## whitespace
     ledq()
+    print('\n') ## whitespace
